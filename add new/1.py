da0fe61d4f09b9b681db164f5f468b10385dd629
lianzi1111 <yjfcyl@163.com>
2018-04-22 16:33:20 +0800

    new

---
+++ add new/1.py
@@ -0,0 +1,21 @@
+# 使用字典实现switch语句
+from __future__ import division
+
+def jia(x,y):
+    return x+y
+
+def jian(x,y):
+    return x-y
+
+def cheng(x,y):
+    return x*y
+
+def chu(x,y):
+    return x/y
+
+operator = {"+":jia,"-":jian,"*":cheng,"/":chu}
+
+def f(x,o,y):
+    print(operator.get(o)(x,y))
+
+f(3,"/",2 )
